67af6d9dcc269bce773bd35482f4bd9c8eece233
Muhammed Fatih Çınar <108172178+fatih045@users.noreply.github.com>
2024-04-05 17:50:50 +0300

    Navbar and routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegisterPageComponent} from "./register-page/register-page.component";
+
+
+
+const routes:Routes = [
+  {path:'login',component:LoginPageComponent},
+  {path:'register',component:RegisterPageComponent},
+
+
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterLink, RouterOutlet} from "@angular/router";
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule,
+    RouterOutlet,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
